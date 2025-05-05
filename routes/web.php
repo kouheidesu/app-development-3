05a390b35fc7267bf8984ecb88cdd17adc570c43
gyarmex <gyarmex@gmail.com>
2025-05-05 10:51:04 +0900

    table追加

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,15 @@
 
 use Illuminate\Support\Facades\Route;
 
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
 Route::get('/', function () {
-    return view('welcome');
+    return 'こんにちは、Laravel！';
+});
+
+
+Route::get('/test.php', function () {
+    return 'こんばんは、Laravel！';
 });

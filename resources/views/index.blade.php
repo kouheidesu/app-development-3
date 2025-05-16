dccc5c7453ad80f49c5cd5ec244c78830e6af3cb
gyarmex <gyarmex@gmail.com>
2025-05-16 21:43:33 +0900

    viewsにアクセス可能にする

---
+++ resources/views/index.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <title>PHPページ</title>
+</head>
+
+<body>
+    <h1>こんにちは、世界！</h1>
+
+    <p>今日は <?php echo date("Y年m月d日"); ?> です。</p>
+
+    <?php
+    $name = "太郎";
+    echo "<p>こんにちは、" . $name . "さん！</p>";
+    ?>
+</body>
+
+</html>

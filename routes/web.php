05253a36ee1e1d11b8e5df88c983eda145278a3f
gyarmex <gyarmex@gmail.com>
2025-05-18 07:53:16 +0900

    views利用可能コードに変更

--- routes/web.php
+++ routes/web.php
@@ -3,18 +3,18 @@
 use Illuminate\Support\Facades\Route;
 
 
-// Route::get('/', function () {
-//     return view('index');
-// });
+Route::get('/', function () {
+    return view('index');
+});
 
 
 // Route::get('/', function () {
 //     return view('index', ['name' => '耕平さん']);
 // });
 
-Route::get('/', function () {
-    return print("hello world");
-});
+// Route::get('/', function () {
+//     return print("hello world");
+// });
 
 
 Route::get('/test.php', function () {

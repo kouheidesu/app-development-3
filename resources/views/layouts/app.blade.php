20511e299c3bbc6b0aaf2e692165c74b4b53d76b
gyarmex <gyarmex@gmail.com>
2025-05-22 06:46:22 +0900

    css反映させるように修正

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,31 +1,30 @@
-<!DnOCTYPE html>
-    <html lang="ja">
+<!DOCTYPE html>
+<html lang="ja">
 
+<head>
+    <meta charset="UTF-8">
+    <title>@yield('title', 'Modern Web')</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <head>
-        <meta charset="UTF-8">
-        <!-- @とは？関数？ -->
-        <title>@yield('title', 'Modern Web')</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        @vite(['resources/css/custom.css', 'resources/js/fade.js'])
-    </head>f
+    {{-- ViteでCSS/JS読み込み --}}
+    @vite(['resources/css/custom.css', 'resources/js/fade.js'])
+</head>
 
-    <body>
-        <header>
-            <h1>Modern Web</h1>
-            <nav>
-                <a href="#about">About</a>
-                <a href="#projects">Projects</a>
-                <a href="#contact">Contact</a>
-            </nav>
-        </header>
+<body>
+    <header>
+        <h1>Modern Web</h1>
+        <nav>
+            <a href="#about">About</a>
+            <a href="#projects">Projects</a>
+            <a href="#contact">Contact</a>
+        </nav>
+    </header>
 
-        @yield('content')
+    @yield('content')
 
-        <footer>
-            <!-- 変数使用可能？ -->
-            &copy; {{ date('Y') }} Modern Web by {{ $name }}
-        </footer>
-    </body>
+    <footer>
+        &copy; {{ date('Y') }} Modern Web by {{ $name }}
+    </footer>
+</body>
 
-    </html>
+</html>
